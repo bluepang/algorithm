d7f79a7bde2bc924fdc1c486e99300d07d8cb3ad
bluepang <772840356@qq.com>
2021-02-18 22:43:10 +0800

    快速排序：先随意定一个基准值，然后把小于基准的放在左边、大于基准的放在右边，最后递归地排序左边和右边的子数组

--- sort.py
+++ sort.py
@@ -1,22 +1,34 @@
-from common import print_exce_time
+from tools import print_exce_time
 
 
 class Sort(object):
-    def __init__(self, list_old):
-        self.list_old = list_old
 
     # 冒泡排序
+    @staticmethod
     @print_exce_time
-    def bubble_sort(self):
-        n = len(self.list_old)
+    def bubble_sort(list_old):
+        n = len(list_old)
         for i in range(n):
             for j in range(n-i-1):
-                if self.list_old[j] > self.list_old[j+1]:
-                    self.list_old[j], self.list_old[j+1] = self.list_old[j+1], self.list_old[j]
+                if list_old[j] > list_old[j+1]:
+                    list_old[j], list_old[j+1] = list_old[j+1], list_old[j]
+        return list_old
+
+    # 快速排序
+    @staticmethod
+    @print_exce_time
+    def quick_sort(list_old):
+        n = len(list_old)
+        if n < 2:
+            return list_old
+        else:
+            pivot = list_old[0]
+            low = [item for item in list_old if item < pivot]
+            high = [item for item in list_old if item > pivot]
+            return Sort.quick_sort(low) + [pivot] + Sort.quick_sort(high)
 
 
 if __name__ == '__main__':
     list_source = [64, 34, 25, 12, 22, 11, 90]
     print('old list is {}'.format(list_source))
-    Sort(list_source).bubble_sort()
-    print('new list is: {}'.format(list_source))
+    print('new list is: {}'.format(Sort.quick_sort(list_source)))
